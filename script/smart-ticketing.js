bc7fd0d161e081c23d3ccf2ca460f4b21675bc30
SohiMuhibe <155249113+SohiMuhibe@users.noreply.github.com>
2024-04-05 16:26:58 +0300

    p.h paribahan added

---
+++ script/smart-ticketing.js
@@ -0,0 +1,13 @@
+function goParibahan(){
+ const main = document.getElementById('main')
+
+    main.scrollIntoView()
+  
+}
+function setBgByID(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-green-400');
+}
+
+
+
